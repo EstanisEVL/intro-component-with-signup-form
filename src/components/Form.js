4044d859c38f5709a3269fb6b63878eb4ca354fc
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2023-01-13 15:51:38 -0300

    Updated README.md

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,5 @@
 import { useForm } from "../hooks/useForm";
 
-  /*
-  Receive an error message when the `form` is submitted if:
-  - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
-  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*
-  */
-
 const initialForm = {
   firstName: '',
   lastName: '',
@@ -45,7 +39,6 @@ const Form = () => {
     handleChange,
     handleSubmit
   } = useForm(initialForm, validationsForm);
-
 
   return(
     <>
